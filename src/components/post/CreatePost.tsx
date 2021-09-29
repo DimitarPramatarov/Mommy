d072f1c0c50fab715974b5d1655698b13831a6d9
Dimitar Pramatarov <dim.pramatarov@gmail.com>
2021-09-29 16:21:24 +0300

    Fix Create Post

--- src/components/post/CreatePost.tsx
+++ src/components/post/CreatePost.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useContext} from 'react';
-import { TextInput, Text, View, NativeSyntheticEvent, TextInputChangeEventData, TouchableOpacity } from 'react-native'
+import {StyleSheet, TextInput, Text, View, NativeSyntheticEvent, TextInputChangeEventData, TouchableOpacity } from 'react-native'
 import { RootStackScreenProps } from '../../../types';
 import UserContext from '../../Context';
 import {addPost} from '../../services/post/PostService';
@@ -30,15 +30,21 @@ const CreatePost = ({navigation} : RootStackScreenProps<"CreatePost">) => {
 
     return(
         <View>
+            <View style={style.top}>
+            <Text>New Article</Text>
+            </View> 
+            <View style={style.download}>
+            </View>
             <View>
-            <Text>
-                Your title
-                </Text>
+           <View>
             <TextInput
+            style={style.title}
                 placeholder="Your title..."
                 value={title}
                 onChange={hadnleTitle}
-            />
+                />
+                <View style={style.line}></View>
+                </View>
             </View>
             <View>
                 <Text>
@@ -58,5 +64,45 @@ const CreatePost = ({navigation} : RootStackScreenProps<"CreatePost">) => {
         </View>
     )
 }
+const style = StyleSheet.create({
+    top: {
+        position: "absolute",
+        width: 375,
+        height: 101,
+        left: 0,
+      top: 0,
+
+    },
+    newArticle: {
+        width:133,
+        height: 33,
+        top: 68,
+        Left: 40,
+    },
+    download: {
+        position: "absolute",
+        width: 32,
+        height: 32,
+        left: 383,
+        top: 68,
+    },
+    title:{
+        position: "absolute",
+        width: 295,
+        height:37,
+        left:40,
+        top:140,
+    },
+    line:{
+        position: 'absolute',
+        left: '10.67%',
+        right: '10.65%',
+        top: '21.43%',
+        bottom: '78.2%'
+    },
+    correctAnswer: {
+        backgroundColor: "#00ff00"
+    }
+});
 
 export default CreatePost
