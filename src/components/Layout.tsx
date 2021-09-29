c9ab5a116563ffd98d18fa79bfa7709696e02f76
Dimitar Pramatarov <dim.pramatarov@gmail.com>
2021-09-29 16:39:17 +0300

    Add layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Layout = () => {
+    
+    return(
+        <View>
+            <Text>LAYOUT!</Text>
+        </View>
+    )
+}
+
+
+export default Layout
