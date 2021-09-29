aa89cc761502bb859952427729d89283fe86a8eb
Dimitar Pramatarov <dim.pramatarov@gmail.com>
2021-09-29 16:38:03 +0300

    Add function and state for search

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import Post from '../components/post/Post';
 import {ScrollView, View, Text, TouchableOpacity} from 'react-native'
 import { RootStackScreenProps } from '../../types';
+import Logout from '../components/auth/Logout';
+import SearchBar from '../components/search/SearchBar';
 
 const HomeScreen = ({navigation} : RootStackScreenProps<"HomeScreen">) => {
+
+    const [searchResult, setSearchResult] = useState<any[]>([]);
+
+    const handleSearchResult = async (searchData: any[]) => {
+        await setSearchResult(searchData)
+    }
+
+    useEffect(() => {
+        handleSearchResult
+        
+    }, [searchResult])
+
 
     const createPost = () => {
         navigation.navigate("CreatePost");
     }
 
+    const myProfile = () => {
+        navigation.navigate("MyProfile");
+    }
+
     return(
         <ScrollView>
+            <SearchBar handleSearchResult={handleSearchResult}/>
             <TouchableOpacity onPress={createPost}>
                 <Text>CreatePost</Text>
                 </TouchableOpacity>
+                <TouchableOpacity onPress={myProfile}>
+                <Text>MyProfile</Text>
+                </TouchableOpacity>
+                <View>
+                    <Logout />
+                </View>
         <View>
-            <Post {...navigation}/>
+            <Post dataFromSearch={searchResult} navigation={navigation}/>
         </View>
         </ScrollView>
     )
